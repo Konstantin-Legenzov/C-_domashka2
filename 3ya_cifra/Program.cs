60935e5b94dba169cf3b9c4b811a34eca97f78e9
Konstantin <legenzov-k@ya.ru>
2022-10-26 09:23:58 +0300

    3ya_cifra

---
+++ 3ya_cifra/Program.cs
@@ -0,0 +1,30 @@
+﻿// выводит 3ю цифру если нет то пишет нет
+
+int N;
+N = new Random().Next(0, 10000);
+
+Console.WriteLine(N);
+
+int i = 100;
+int i2 = 1000;
+int i3 = 10000;
+
+int result;
+int result2;
+int result3;
+
+if (N < i)
+{
+    Console.WriteLine("Нет 3го числа");
+}
+if (N < i2 && N > i)
+{
+    result = N % 10;
+    Console.WriteLine(result);
+}
+if (N < i3 && N > i2)
+{
+    result2 = N % 100;
+    result3 = result2 / 10;
+    Console.WriteLine(result3);
+}
