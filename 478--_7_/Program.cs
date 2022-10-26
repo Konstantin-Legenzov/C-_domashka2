3b0e87e2a3e284db4b357f2c6258327d88fc4088
Konstantin <legenzov-k@ya.ru>
2022-10-26 08:49:55 +0300

    zadacha_001

---
+++ 478--_7_/Program.cs
@@ -0,0 +1,10 @@
+﻿// из трехзначного числа показывает вторую цифру
+
+int rand = new Random().Next(100, 1000);
+
+Console.WriteLine(rand);
+
+int a = rand / 10;
+int b = a % 10;
+
+Console.WriteLine(b);
